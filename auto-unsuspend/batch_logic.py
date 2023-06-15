fc8195ac1070e3cc883d04c19996b8733ba52107
jcgy <17726751+jcgy@users.noreply.github.com>
2023-06-15 04:27:55 +0100

    Remove debugging showInfo() calls

--- auto-unsuspend/batch_logic.py
+++ auto-unsuspend/batch_logic.py
@@ -3,7 +3,6 @@ from anki.notes import Note
 from . import const
 from aqt.utils import showInfo
 
-
 def unsuspend_cards():
 	# Get current rules set
 	META = const.CONFIG
@@ -15,16 +14,11 @@ def unsuspend_cards():
 
 	# Load current config
 	META = const.CONFIG
-	#showInfo("Unsuspend is running")
 
 	for rule_name, rule in const.META.get("config", {}).get("Rules", {}).items():
-		#showInfo(f"rule_name: {rule_name}")
 		tag = rule.get("tag")
-		#showInfo(f"tag: {tag}")
 		n = rule.get("cards_count")
-		#showInfo(f"n: {n}")
 		active = rule.get("active")
-		#showInfo(f"active: {active}")
 		# Set a checkpoint so batch unsuspend can be undone if needed
 		mw.checkpoint(f"Unsuspend Cards")
 		# Check if the rule is currently activated
@@ -34,7 +28,6 @@ def unsuspend_cards():
 				# Sort by their ID (which is equivalent to sorting by creation date)
 				card_ids.sort()
 				# Unsuspend the cards
-				#showInfo(f"card_ids to unsuspend: {card_ids[:n]}")
 				mw.col.sched.unsuspendCards(card_ids[:n])
 		# Reset the collection to update UI
 		mw.reset()
